10ee0b23399affd7976cc039fa11ea9485e6543b
Imeth <imethdesilva@gmail.com>
2024-07-18 10:25:55 +0530

    content page - imeth

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -3,6 +3,43 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const lightboxImg = document.getElementById('lightbox-img');
     const lightboxCaption = document.getElementById('lightbox-caption');
     const close = document.getElementsByClassName('close')[0];
+    const font_button = document.querySelectorAll('.font-button')
+    const text_bg_color = document.querySelectorAll('.text-bg-color');
+    const overlays = document.querySelectorAll('.overlay');
+
+
+    overlays.forEach(overlay => {
+        overlay.addEventListener('click', function(event) {
+        
+            if (event.target.classList.contains('font-button')) {
+                const fontName = event.target.dataset.font;
+                
+              
+                const description = overlay.querySelector('.description');         
+                // Remove previous font classes
+                description.classList.remove('Georgia', 'Times-New-Roman', 'Spartan');
+                
+                // Add new font class based on data-font attribute
+                description.classList.add(fontName);
+            }
+        });
+
+        // Background color buttons event listener
+        overlay.addEventListener('click', function(event) {
+            // Check if the clicked element is a text-bg-color button
+            if (event.target.classList.contains('text-bg-color')) {
+                const colorName = event.target.dataset.color;
+                
+                const description = overlay.querySelector('.description');
+                
+                // Remove previous background color classes
+                description.classList.remove('red', 'green', 'blue', 'black');
+                
+                // Add new background color class based on data-color attribute
+                description.classList.add(colorName);
+            }
+        });
+    });
 
     document.querySelectorAll('.gallery_item img').forEach(img => {
         img.addEventListener('click', () => {
@@ -22,3 +59,5 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
     });
 });
+
+
